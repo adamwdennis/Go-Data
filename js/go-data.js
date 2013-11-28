e0136ccc2e7505538fa631e9a1d9a879cbd63ba8
Neal Stewart <neal@goinstant.com>
2013-11-28 14:55:28 -0400

    Remove node when removed on platform.

--- js/go-data.js
+++ js/go-data.js
@@ -19,6 +19,8 @@ platform.connect(function(err) {
   var roomObj = platform.room(roomName);
   window.roomObj = roomObj;
 
+  var tree;
+
   roomObj.join(function(err) {
     if (err) {
       throw err;
@@ -26,7 +28,8 @@ platform.connect(function(err) {
 
     var rootKey = roomObj.key('/');
     rootKey.get(setupTree);
-    roomObj.on('set', update);
+    rootKey.on('set', update);
+    rootKey.on('remove', { bubble:true, listener: keyRemoved });
   });
 
   function setupTree(err, val) {
@@ -37,7 +40,7 @@ platform.connect(function(err) {
     var str = convertJSONToLists(val);
 
     $("#godata").append('<ul>' + str + '</ul>');
-    var tree = $('#godata').jstree({
+    tree = $('#godata').jstree({
       "plugins": [
         "themes",
         "html_data",
@@ -127,14 +130,26 @@ platform.connect(function(err) {
     */
   }
 
-  function update(val, context) {
+  function update() {
     // find the element
-    var keys = context;
 
     // update the element
     // expand the tree up to the element
     // scroll to the element
     // highlight the element
+  }
+
+  function keyRemoved(val, context) {
+    var node = findNode(context.key);
+    if (!node) {
+      return;
+    }
+
+    removeAndPrune(node.get(0));
+  }
+
+  function removeAndPrune(node) {
+    tree.jstree('remove', node);
   }
 
   function remove(node) {
@@ -146,6 +161,24 @@ platform.connect(function(err) {
       tree.remove(node);
     });
   }
+
+  function findNode(keyName) {
+    var keyNames = keyName.split('/').slice(1);
+
+    var selectorKeyNames = keyNames.map(function(key) {
+      return 'li[data-key-name=' + key + ']';
+    });
+
+    var selector = selectorKeyNames.join(' ');
+
+    var node = tree.find(selector);
+
+    if (node.length) {
+      return node.eq(0);
+    } else {
+      return null;
+    }
+  }
 });
 
 function convertJSONToLists(o, str) {
@@ -153,7 +186,7 @@ function convertJSONToLists(o, str) {
   var type = typeof o;
   if (type == "object") {
     for (var key in o) {
-      str += '<li class="key" id="' + key + '">';
+      str += '<li class="key" data-key-name="' + key + '">';
       str += '<a class="key">/' + key + '</a>';
       str += '<ul>';
       str = convertJSONToLists(o[key], str);
@@ -162,19 +195,18 @@ function convertJSONToLists(o, str) {
     }
 
   } else {
-    str += '<li id="' + o + '" class="value"><a>' + o + '</a></li>';
+    str += '<li data-key-name="' + o + '" class="value"><a>' + o + '</a></li>';
   }
 
   return str;
 }
-
 
 function getKeyName(node) {
   var name = "";
   node = $(node);
   do {
     if (node.prop('tagName') == 'LI' && node.hasClass('key')) {
-      name = '/' + node.attr('id') + name;
+      name = '/' + node.data('key-name') + name;
     }
 
     node = node.parent();
