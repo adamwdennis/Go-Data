490254bfc751686c109d59b451ff019e8ad7cf6e
adamwdennis <adam.dennis@gmail.com>
2013-11-28 09:57:45 -0400

    pushing for hackathon

--- js/go-data.js
+++ js/go-data.js
@@ -1,6 +1,7 @@
 // Connect URL
-//var url = "https://goinstant.net/adamwdennis/test";//prompt("GoInstant App URL?");
-var url = prompt("GoInstant App URL?");
+var url = "https://goinstant.net/goinstant-services/docs";
+var roomName = 'chat';//prompt("Which Room?");
+//var url = prompt("GoInstant App URL?");
 
 var platform = new goinstant.Platform(url);
 
@@ -15,30 +16,27 @@ platform.connect(function(err) {
     var type = typeof o; 
     if (type == "object") {
       for (var key in o) {
-      str += '<li><a>KEY:' + key + '</a><ul>';
+      str += '<li id="' + key + '"><a class="key">/' + key + '</a><ul>';
         js_traverse(o[key]);
         str += "</ul></li>";
       }
     } else {
-      str += "<li><a>OBJ:" + o + "</a></li>";
+      str += '<li id="' + o + '" class="value"><a>' + o + '</a></li>';
     }
   }
 
-  var roomName = prompt("Which Room?");
-  //var roomName = "example";//prompt("Which Room?");
   var roomObj = platform.room(roomName);
   roomObj.join(function(err) {
     roomObj.key('/').get(function(err, val, context) {
       js_traverse(val);
-      $("#container").append('<ul>' + str + '</ul>');
-      $('#container').jstree({
+      $("#godata").append('<ul>' + str + '</ul>');
+      $('#godata').jstree({
         "plugins": [
           "themes",
           "html_data",
           "ui",
           "crrm",
           "hotkeys",
-          "checkbox",
           "types",
           "contextmenu"
         ],
@@ -46,7 +44,20 @@ platform.connect(function(err) {
           "theme": "apple",
           "dots": true,
           "icons": true
-        },
+        }
+      }).bind("select_node.jstree", function (e, data) {
+        console.log("STUFF:",e,data);
+        console.log("ID:",data.rslt.obj.attr("id"));
+      });
+    });
+    roomObj.key('/messages').get(function(err,val, context) {
+      Object.keys(val).forEach(function(key) {
+        console.log(key, val[key].text);
+      });
+      roomObj.key('/messages/1384795411415_628759779').remove(function(err) {
+        if (err) {
+          throw err;
+        }
       });
     });
   });
